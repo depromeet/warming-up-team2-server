ed909c15813fdcecc93a1996047a8dad647c825e
junbly <junhyeon47@gmail.com>
2019-10-20 02:02:15 +0900

    init project

---
+++ src/main/java/com/depromeet/booboo/BoobooApplication.java
@@ -0,0 +1,12 @@
+package com.depromeet.booboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoobooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoobooApplication.class, args);
+	}
+}
---
+++ src/test/java/com/depromeet/booboo/BoobooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.booboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoobooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
