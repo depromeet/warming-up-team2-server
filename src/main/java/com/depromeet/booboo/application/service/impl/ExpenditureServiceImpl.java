428ca96f4d98da0bcf676b5a41c1e6b580012eac
전해성 <seong0428@gmail.com>
2019-11-03 05:31:00 +0900

    feat #27 - 지출 내역 이미지 업로드 기능을 추가합니다

--- src/main/java/com/depromeet/booboo/application/service/impl/ExpenditureServiceImpl.java
+++ src/main/java/com/depromeet/booboo/application/service/impl/ExpenditureServiceImpl.java
@@ -1,5 +1,6 @@
 package com.depromeet.booboo.application.service.impl;
 
+import com.depromeet.booboo.application.adapter.storage.StorageAdapter;
 import com.depromeet.booboo.application.assembler.ExpenditureAssembler;
 import com.depromeet.booboo.application.service.ExpenditureService;
 import com.depromeet.booboo.domain.expenditure.Expenditure;
@@ -10,9 +11,12 @@ import com.depromeet.booboo.domain.member.MemberRepository;
 import com.depromeet.booboo.ui.dto.ExpenditureRequest;
 import com.depromeet.booboo.ui.dto.ExpenditureResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+
+import java.io.InputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class ExpenditureServiceImpl implements ExpenditureService {
     private final MemberRepository memberRepository;
     private final ExpenditureRepository expenditureRepository;
     private final ExpenditureAssembler expenditureAssembler;
+    private final StorageAdapter storageAdapter;
 
     @Override
     @Transactional
@@ -36,6 +41,23 @@ public class ExpenditureServiceImpl implements ExpenditureService {
                 expenditureRequest.getDescription()
         );
         expenditureRepository.save(expenditure);
+        return expenditureAssembler.toExpenditureResponse(expenditure);
+    }
+
+    @Override
+    @Transactional
+    public ExpenditureResponse updateImage(Long memberId, Long expenditureId, MediaType mediaType, InputStream inputStream) {
+        Assert.notNull(memberId, "'memberId' must not be null");
+        Assert.notNull(expenditureId, "'expenditureId' must not be null");
+        Assert.notNull(inputStream, "'inputStream' must not be null");
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new ExpenditureException("member not found. memberId:" + memberId));
+        Expenditure expenditure = expenditureRepository.findByMemberAndExpenditureId(member, expenditureId)
+                .orElseThrow(() -> new ExpenditureException("expenditure not found. memberId:" + memberId + ", expenditureId:" + expenditureId));
+
+        String imageUrl = storageAdapter.save(mediaType, inputStream);
+        expenditure.updateImageUrl(imageUrl);
         return expenditureAssembler.toExpenditureResponse(expenditure);
     }
 }
