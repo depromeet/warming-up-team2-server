0d78d46c50fbcf9940be566515680f6cafa7d8f3
Haeseong Jeon <seong0428@gmail.com>
2019-11-08 21:04:37 +0900

    refactor #69 - 파일업로드 실패시 상위 exception 이 로그에서 누락되는 현상 해결

--- src/main/java/com/depromeet/booboo/application/adapter/storage/StorageException.java
+++ src/main/java/com/depromeet/booboo/application/adapter/storage/StorageException.java
@@ -4,4 +4,8 @@ public class StorageException extends RuntimeException {
     public StorageException(String message) {
         super(message);
     }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
